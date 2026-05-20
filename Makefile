011b2f379dabb2cb72e276436cdfe4cd618e52df
Aleksandra Smolniakova <smolnyakovaae@gmail.com>
2026-05-20 17:56:45 +0200

    now elk is working

--- Makefile
+++ Makefile
@@ -7,14 +7,6 @@ prep:
 
 build:
 	@$(COMPOSE) up -d --build
-	@echo "Waiting for Kibana..."
- 	@until curl -s -o /dev/null http://localhost:5601/api/status; do sleep 5; done
- 	@echo "Kibana is ready. Importing dashboard..."
- 	@curl -X POST "http://localhost:5601/api/saved_objects/_import?overwrite=true" \
-		-H "kbn-xsrf: true" \
-		--form file=@infra/elk/kibana/dashboard.ndjson
- 	@echo "\nDashboard imported."
-
 up:
 	@$(COMPOSE) up -d
 
--- infra/elk/kibana/setup-kibana.sh
+++ infra/elk/kibana/setup-kibana.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-KIBANA_URL="http://kibana:5601"
+KIBANA_URL="http://localhost:5601"
 NDJSON_FILE="/tmp/dashboard.ndjson"
 
+/usr/local/bin/kibana-docker &
+
 echo "Waiting for kibana..."
-until curl -s -o /dev/null "$KIBANA_URL/api/status"; do
+until curl -s -o /dev/null -w "%{http_code}" "$KIBANA_URL/api/status" | grep -q "200"; do
 	sleep 5
 done
 echo "Kibana is ready!"
 
-curl -X POST "$KIBANA_URL/api/saved_obdjects/_import?overwrite=true" \
+curl -X POST "$KIBANA_URL/api/saved_objects/_import?overwrite=true" \
 	-H "kbn-xsrf: true" \
-	--form file=@"NDJSON_FILE"
+	-H "Content-Type: multipart/form-data" \
+ 	--form file=@"$NDJSON_FILE"
 
 echo ""
 echo "Dashboard imported!"
+
+wait
